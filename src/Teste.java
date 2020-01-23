9ee51606ad16792bbd3ce58826b2db82a899832f
andressarfn <andressarfn@hotmail.com>
2020-01-23 14:04:19 -0300

    conta

---
+++ src/Teste.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("***TESTE ContaBancaria***");
+		
+		ContaBancaria contaSimples = new ContaBancaria();
+		contaSimples.setNomeCliente("Cliente Conta Simples");
+		contaSimples.setNumConta("11111");
+		
+		System.out.println(contaSimples);
+	}
+	
+	
+}
